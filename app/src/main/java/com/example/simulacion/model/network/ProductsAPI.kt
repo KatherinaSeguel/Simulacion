e32e3960a91517e6b0daece9264e1e0a839b207a
katthy <you@example.com>
2020-10-24 13:57:44 -0300

    primer commit interface lista Retrofit Entity y Dao listo database lista Repository Listo itemlist listo Adapter y ViewModel Listo Listo FirstFragment Muestra los datos en ReciclerView Vamos al Segundo Fragmento

--- app/src/main/java/com/example/simulacion/model/network/ProductsAPI.kt
+++ app/src/main/java/com/example/simulacion/model/network/ProductsAPI.kt
@@ -2,9 +2,15 @@ package com.example.simulacion.model.network
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ProductsAPI {
-    //Detalle de los product
-    @GET("details")
-    fun getDataFromApi() : Call<productsItem>
+    // los product
+    @GET("products/")
+    fun getDataFromApi() : Call<products>
+
+    //Buscar un fruto
+    @GET("products/")
+    fun getDataFromApiCorutines(@Query("produ") mProducts:String): retrofit2.Response<products>
+
 }
